a55a283302051873d657fd52fe77292cadbabe9d
CristinaMSousa <cristina.msou@gmail.com>
2023-10-18 23:29:36 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVIDADES NO HOMEM DE FERRO 5",
+        "description": "Marvel anuncia um novo filme do Homem de Ferro",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPr9JPIto-M9hBmDWx8mlu22RcAmU2uJ11lw&usqp=CAU"
+    },
+    {
+        "id": "2",
+        "title": "Nova série é anunciada no Disney plus",
+        "description": "blabla bla",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/08/WandaVision-Falcao-e-Loki-1024x576.jpg"
+    }
+]
